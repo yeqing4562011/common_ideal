53e9d3eb26b78b7f4d5480b7f6333e8b8b5d4b34
YeQing <wsmbaiwo@126.com>
2013-08-26 09:14:14 +0800

    commit

--- src/main/java/com/ideal/common/service/interf/RMIObject.java
+++ src/main/java/com/ideal/common/service/interf/RMIObject.java
@@ -7,30 +7,30 @@ import java.io.Serializable;
  * @Description: TODO
  * @author yq
  * @date 2013年8月5日 下午4:17:06
- * 
+ *
  */
 public class RMIObject implements Serializable{
-	
+
 	/**
 	* @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
 	*/
-	
+
 	private static final long serialVersionUID = 1L;
 	private String name;
 
-	/** 
-	* @return name 
-	*/ 
-	
-	
+	/**
+	* @return name
+	*/
+
+
 	public String getName() {
 		return name;
 	}
 
-	/** 
-	* @param name 要设置的 name 
-	*/ 
-	
+	/**
+	* @param name 要设置的 name
+	*/
+
 	public void setName(String name) {
 		this.name = name;
 	}
